ae622afb802b9a20282cc98f1a69e365816d5bde
Henrik Renlund <henrikrenlund@gmail.com>
2025-01-13 15:08:54 +0100

    trying to clean up baseline descriptives

--- R/fncs-baseline.R
+++ R/fncs-baseline.R
@@ -8,33 +8,33 @@
 NULL
 
 ##' @rdname baseline-standard
-##' @details mean_sd.bl: (weighted) mean and standard deviation as single string
+##' @details real.bl: (weighted) median and interquartile range
 ##' @export
-mean_sd.bl <- function(x, weight = NULL, ...){
-    r <- mean_sd(x = x, weight = weight, ...)
-    data.frame(Variable = di.Variable(x, ...),
-               Summary = sprintf("%s (%s)", dform.num(r$Mean), dform.num(r$SD)),
-               Summary.info = "Numeric variable: Mean(SD)")
-}
-attr(mean_sd.bl, "meta") <- c("Summary.info", "Variable")
-
-##' @rdname baseline-standard
-##' @details midiqr.bl: (weighted) median and interquartile range as single string
-##' @export
-mediqr.bl <- function(x, weight = NULL, ...){
+real.bl <- function(x, weight = NULL, ...){
     q <- quartiles(x = x, weight = weight)
     data.frame(Variable = di.Variable(x, ...),
                Summary = sprintf("%s (%s - %s)", dform.num(q$Q2),
                                  dform.num(q$Q1), dform.num(q$Q2)),
                Summary.info = "Numeric variable: Median (Q1-Q3)")
 }
-attr(mean_sd.bl, "meta") <- c("Summary.info", "Variable")
+attr(real.bl, "meta") <- c("Summary.info", "Variable")
 
 ##' @rdname baseline-standard
-##' @details bnry.count_prop.bl: (weighted) count and percent of
+##' @details real2.bl: (weighted) mean and standard deviation
+##' @export
+real2.bl <- function(x, weight = NULL, ...){
+    r <- mean_sd(x = x, weight = weight, ...)
+    data.frame(Variable = di.Variable(x, ...),
+               Summary = sprintf("%s (%s)", dform.num(r$Mean), dform.num(r$SD)),
+               Summary.info = "Numeric variable: Mean(SD)")
+}
+attr(real2.bl, "meta") <- c("Summary.info", "Variable")
+
+##' @rdname baseline-standard
+##' @details bnry.bl: (weighted) count and percent of
 ##'     non-reference value as single string
 ##' @export
-bnry.count_prop.bl <- function(x, weight = NULL, ...){
+bnry.bl <- function(x, weight = NULL, ...){
     r <- bnry.count_prop(x = x, weight = weight, ...)
     data.frame(Variable = di.Variable(x, ...),
                Summary = sprintf("%s (%s%%)",
@@ -42,13 +42,13 @@ bnry.count_prop.bl <- function(x, weight = NULL, ...){
                                  dform.num(100 * r$Proportion)),
                Summary.info = "Categorical variable: Count (Percent)")
 }
-attr(bnry.count_prop.bl, "meta") <- c("Variable", "Summary.info")
+attr(bnry.bl, "meta") <- c("Variable", "Summary.info")
 
 ##' @rdname baseline-standard
-##' @details catg.count_prop.bl: (weighted) count and proportion of
+##' @details catg.bl: (weighted) count and proportion of
 ##'     categorical levels as single string
 ##' @export
-catg.count_prop.bl <- function(x, weight = NULL, ...){
+catg.bl <- function(x, weight = NULL, ...){
     r <- catg.count_prop(x = x, weight = weight, ...)
     data.frame(Variable = di.Variable(x, ...),
                Summary = sprintf("%s (%s%%)",
@@ -56,7 +56,7 @@ catg.count_prop.bl <- function(x, weight = NULL, ...){
                                  dform.num.vec(100 * r$Proportion)),
                Summary.info = "Categorical variable: Count (Percent)")
 }
-attr(catg.count_prop.bl, "meta") <- c("Variable", "Summary.info")
+attr(catg.bl, "meta") <- c("Variable", "Summary.info")
 
 ##' @rdname baseline-standard
 ##' @details lcat.bl: number of unique non-missing values in x
@@ -81,9 +81,9 @@ date.bl <- function(x, ...){
 attr(date.bl, "meta") <- c("Variable", "Summary.info")
 
 ##' @rdname baseline-standard
-##' @details eventrate.bl: events; rate
+##' @details surv.bl: events; rate
 ##' @export
-eventrate.bl <- function(time, event, weight = NULL, time.unit = NULL, ...){
+surv.bl <- function(time, event, weight = NULL, time.unit = NULL, ...){
     r <- eventrate(time = time, event = event, weight = weight,
                    time.unit = time.unit, ...)
     data.frame(Variable = di.Variable(event, ...),
@@ -92,4 +92,4 @@ eventrate.bl <- function(time, event, weight = NULL, time.unit = NULL, ...){
                                  dform.num(r$Rate)),
                Summary.info = "Time-to-event variable: events; rate")
 }
-attr(eventrate.bl, "meta") <- c("Variable", "Summary.info")
+attr(surv.bl, "meta") <- c("Variable", "Summary.info")
