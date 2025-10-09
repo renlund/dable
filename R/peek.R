66a27da297a98ecfb0deb89927f9354590e00178
Henrik Renlund <henre358@user.uu.se>
2025-10-09 16:13:02 +0200

    peek adjustment

--- R/peek.R
+++ R/peek.R
@@ -1,19 +1,25 @@
 ##' peek at an object
 ##'
-##' a printing method for interactive use
+##' a printing method for interactive use only - do not use in scripts as it is
+##' likely to be unstable
 ##' @param x object to take a peek at
 ##' @param right logical, passed to print
 ##' @param ... passed to print
 ##' @export
 peek <- function(x, right = FALSE, ...){
     t <- attr(x, "type")
-    if(t == "baseline"){
-        ii <- unique(grep("info", x = names(x), value = TRUE))
-        for(i in ii) x <- dable_prune(x, rm = i)
-        print.data.frame(dable_format(x, output = "console"), right = right, ...)
-    } else if(t %in% c("real", "catg", "bnry", "lcat", "surv", "date")){
-        print.data.frame(x, right = right, ...)
+    if(is.null(t)){
+        print(x)
     } else {
-        print(x, ...)
+        if(t == "baseline"){
+            ii <- unique(grep("info", x = names(x), value = TRUE))
+            for(i in ii) x <- dable_prune(x, rm = i)
+            ## XK column names should be fitted from attr(x, "part")
+            print.data.frame(dable_format(x, output = "console"), right = right, ...)
+        } else if(t %in% c("real", "catg", "bnry", "lcat", "surv", "date")){
+            print.data.frame(x, right = right, ...)
+        } else {
+            print(x, ...)
+        }
     }
 }
