2fffd344870aa700492c6f203159aa09cf4bd0d3
Henrik Renlund <henre358@user.uu.se>
2025-12-17 11:20:37 +0100

    updates to peek

--- R/peek.R
+++ R/peek.R
@@ -3,23 +3,50 @@
 ##' a printing method for interactive use only - do not use in scripts as it is
 ##' likely to be unstable
 ##' @param x object to take a peek at
-##' @param right logical, passed to print
-##' @param ... passed to print
+##' @param bl.term logical; include term in baseline tables
 ##' @export
-peek <- function(x, right = FALSE, ...){
+peek <- function(x, bl.term = FALSE){
     t <- attr(x, "type")
     if(is.null(t)){
-        print(x)
+        x
     } else {
         if(t == "baseline"){
             ii <- unique(grep("info", x = names(x), value = TRUE))
             for(i in ii) x <- dable_prune(x, rm = i)
-            ## XK column names should be fitted from attr(x, "part")
-            print.data.frame(dable_format(x, output = "console"), right = right, ...)
+            if("term" %in% names(x)) x <- dable_prune(x, rm = "term")
+            part <- attr(x, "part")
+            i <- which(part != "meta")
+            nm <- sub("desc:", "", part[i])
+            names(x)[i] <- nm
+            dable_format(x, output = "console")
         } else if(t %in% c("real", "catg", "bnry", "lcat", "surv", "date")){
-            print.data.frame(x, right = right, ...)
+            x
         } else {
-            print(x, ...)
+            x
         }
     }
 }
+
+## #-# @param right logical, passed to print
+## #-# @param ... passed to print
+## peek <- function(x, bl.term = FALSE, right = FALSE, ...){
+##     t <- attr(x, "type")
+##     if(is.null(t)){
+##         print(x)
+##     } else {
+##         if(t == "baseline"){
+##             ii <- unique(grep("info", x = names(x), value = TRUE))
+##             for(i in ii) x <- dable_prune(x, rm = i)
+##             if("term" %in% names(x)) x <- dable_prune(x, rm = "term")
+##             part <- attr(x, "part")
+##             i <- which(part != "meta")
+##             nm <- sub("desc:", "", part[i])
+##             names(x)[i] <- nm
+##             print.data.frame(dable_format(x, output = "console"), right = right, ...)
+##         } else if(t %in% c("real", "catg", "bnry", "lcat", "surv", "date")){
+##             print.data.frame(x, right = right, ...)
+##         } else {
+##             print(x, ...)
+##         }
+##     }
+## }
